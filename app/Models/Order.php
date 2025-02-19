58ae07078acd52b3e987010e3ddedfe7586ed3ae
Tommaso Massenz <tommaso.massenz.tm@gmail.com>
2025-02-19 11:38:58 +0100

    migration users

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,8 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class Order extends Model
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
+    /** @use HasFactory<\Database\Factories\OrderFactory> */
     use HasFactory;
 }
---
+++ database/migrations/2025_02_19_100018_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id_utente')->unique();
+            $table->string('username', 50)->unique();
+            $table->string('email', 100)->unique();
+            $table->string('password', 50);
+            $table->string('nome',40);
+            $table->string('cognome',40);
+            $table->date('ddn');
+            $table->date('ddr');
+            $table->string('indirizzo', 100);
+            $table->string('numtelefono', 15);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
