ce2c1d508e7320ae22c8295af9c28ffe84c476ff
Tommaso Massenz <tommaso.massenz.tm@gmail.com>
2025-02-20 09:46:23 +0100

    user resource

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
 
 class UserController extends Controller
 {
@@ -12,19 +13,7 @@ class UserController extends Controller
     public function index()
     {
         $user = User::all();
-        return response()->json([
-            'status' => true,
-            'user' => $user
-        ]);
-    }
-
-    
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return UserResource::collection($user);
     }
 
     /**
---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Http\Requests\UserRequest;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(UserRequest $userRequest): array
+    {
+        return [
+            'username' => $this->username,
+            'email' => $this->email,
+
+        ];
+    }
+}
